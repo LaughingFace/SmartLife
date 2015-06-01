cb52e3c603c9b7dfd63cefeca5fe6ed542f377ab
zihao <zihao131125@gmail.com>
2015-06-01 23:11:48 +0800

    ...

--- microWash/src/com/laughingFace/microWash/ui/view/SlidingMenu.java
+++ microWash/src/com/laughingFace/microWash/ui/view/SlidingMenu.java
@@ -1,10 +1,7 @@
 package com.laughingFace.microWash.ui.view;
 
-import android.annotation.TargetApi;
 import android.content.Context;
-import android.os.Build;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
@@ -20,14 +17,17 @@ public class SlidingMenu extends FrameLayout {
 
     private GestureDetector mGestureDetector;
     private View menu;
+    private View content;
     private View menuContent;
     private int showingWidth =0;//菜单完全显示的宽度
     private int offsetX = 0;
     private boolean isShowing = false;
     private LinearLayout.LayoutParams layoutParams;
+    private long currentbg = 0x00000000;//默认全透明
+    private final long MINALPHA = 0x80;//透明度的最低值（值越高越透明 八位十六进制的最高两位代表透明度）
+    private long maskColor =0x202020 ;//菜单出现后周围遮罩颜色
     private final int k = 3;//“劲度系数”控制菜单完全显示后继续拉动菜单的难度（参考胡克定律：f=k.x）
     private SlidingMenuListenear slidingMenuListenear;
-
     private ImageView menuHandle;
 
     public SlidingMenu(Context context, AttributeSet attrs) {
@@ -40,24 +40,22 @@ public class SlidingMenu extends FrameLayout {
         mGestureDetector = new GestureDetector(context,new MySimpleGL());
         this.setLongClickable(true);//没这句的话不能触发滑动事件
         this.post(new Runnable() {
-            @TargetApi(Build.VERSION_CODES.HONEYCOMB)
             @Override
             public void run() {
                 menu = SlidingMenu.this.findViewById(R.id.menu);
+                content = SlidingMenu.this.findViewById(R.id.content);
                 menuContent = SlidingMenu.this.findViewById(R.id.menu_content);
                 menuHandle = (ImageView) SlidingMenu.this.findViewById(R.id.menu_handle);
-                /**
-                 * 点击把手显示/关闭菜单
-                 */
                 menuHandle.setOnClickListener(new OnClickListener() {
                     @Override
                     public void onClick(View v) {
                         if (isShowing()) {
                             SlidingMenu.this.hide();
+
                         } else {
+
                             SlidingMenu.this.show();
                         }
-
                     }
                 });
                 showingWidth = menuContent.getWidth();
@@ -75,7 +73,7 @@ public class SlidingMenu extends FrameLayout {
                 switch (event.getAction()) {
                     case MotionEvent.ACTION_UP:
                         if (offsetX < showingWidth / 2) {
-                            show();
+                            show();//完全显示菜单
                         } else {
                             hide();
                         }
@@ -85,14 +83,12 @@ public class SlidingMenu extends FrameLayout {
             }
         });
     }
-
     private class MySimpleGL extends GestureDetector.SimpleOnGestureListener{
         @Override
         public boolean onDown(MotionEvent e) {
             return super.onDown(e);
         }
 
-        @TargetApi(Build.VERSION_CODES.HONEYCOMB)
         @Override
         public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
             offsetX += distanceX;
@@ -112,7 +108,12 @@ public class SlidingMenu extends FrameLayout {
              * 通过scale这个比例计算出当前背景的透明度
              */
             float scale = ((float)showingWidth - (float)offsetX) / (float)showingWidth;//菜单显示出来部分宽度与总宽度之比
-            SlidingMenu.this.setAlpha(0.5f+scale);
+            currentbg =  0x01000000l * (long)(MINALPHA*scale)+maskColor;
+            if(currentbg > 0x01000000l * MINALPHA+maskColor){
+                currentbg = 0x01000000l * MINALPHA+maskColor;
+            }
+            menu.setBackgroundColor((int) currentbg);
+
             return super.onScroll(e1, e2, distanceX, distanceY);
         }
 
@@ -121,7 +122,6 @@ public class SlidingMenu extends FrameLayout {
             return super.onFling(e1, e2, velocityX, velocityY);
         }
 
-        @TargetApi(Build.VERSION_CODES.HONEYCOMB)
         @Override
         public boolean onSingleTapUp(MotionEvent e) {
             hide();
@@ -129,34 +129,37 @@ public class SlidingMenu extends FrameLayout {
         }
     }
 
-    /**
-     * 完全显示菜单
-     */
-    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
     public void show(){
         offsetX = 0;//完全显示菜单
+        menu.setBackgroundColor((int) (MINALPHA * 0x01000000 + maskColor));//背景设置为半透明
+        content.setVisibility(INVISIBLE);
         if (null != slidingMenuListenear) {
             slidingMenuListenear.onOpened();
         }
 
         layoutParams.width = showingWidth;
         menuContent.setLayoutParams(layoutParams);
-        menu.scrollTo(offsetX,0);
+        menu.scrollTo(offsetX, 0);
         isShowing = true;
     }
 
-    /**
-     * //隐藏菜单
-     */
     public void hide(){
-        offsetX = showingWidth;
+        offsetX = showingWidth;//隐藏菜单
+        menu.setBackgroundColor(0x00000000);//背景设置为全透明
+        content.setVisibility(VISIBLE);
+
         layoutParams.width = showingWidth;
         menuContent.setLayoutParams(layoutParams);
         menu.scrollTo(offsetX, 0);
         isShowing = false;
         if(null != slidingMenuListenear){
+
             slidingMenuListenear.onClosed();
         }
+    }
+
+    public boolean isShowing() {
+        return isShowing;
     }
 
     public SlidingMenuListenear getSlidingMenuListenear() {
@@ -167,12 +170,8 @@ public class SlidingMenu extends FrameLayout {
         this.slidingMenuListenear = slidingMenuListenear;
     }
 
-    public boolean isShowing() {
-        return isShowing;
-    }
-
     public interface SlidingMenuListenear{
-        void onClosed();
-        void onOpened();
+         void onClosed();
+         void onOpened();
     }
 }
